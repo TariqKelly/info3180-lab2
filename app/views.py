6022bee54188c0160a38459fa83fb9b5b8fe7ebf
Tariq Kelly <tariqkelly@gmail.com>
2022-02-11 22:00:18 -0500

    complete flask app

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -23,6 +24,10 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', date_joined = format_date_joined(datetime.date(2021, 2, 9)))
 
 
 ###
@@ -52,6 +57,10 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+def format_date_joined(date):
+    formatted_date = date.strftime('%B %Y')
+    return f'Joined {formatted_date}'
 
 
 if __name__ == '__main__':
